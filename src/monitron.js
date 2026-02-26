7cad5d552b269b38911b672343e9714faa452853
nastasagr <alexanstgr@gmail.com>
2026-02-26 19:16:35 +0200

    ignore mouse events

--- src/monitron.js
+++ src/monitron.js
@@ -3,18 +3,20 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 350,
-    height: 220,
+    width: 310,
+    height: "auto",
     frame: false,
     transparent: true,
     backgroundColor: "#00000000",
+
     alwaysOnTop: true,
+    maximizable: false,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
-
+  win.setIgnoreMouseEvents(false);
   win.loadFile(path.join(__dirname, "monitron.html"));
 }
 
