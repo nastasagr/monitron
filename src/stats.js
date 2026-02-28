5b5ffe5a3cca8c2090a3387bc92227360bdf9b4d
nastasagr <alexanstgr@gmail.com>
2026-02-28 22:27:16 +0200

    object function mapper

--- src/stats.js
+++ src/stats.js
@@ -1,34 +1,44 @@
 const { shell } = require("electron");
 const si = require("systeminformation");
+const { updateElement } = require("./helper");
 
-// const github = document.getElementById("github");
+const toGB = (bytes) => Number((bytes / 1024 / 1024 / 1024).toFixed(2));
 
-// /**
-//  *   Project Repository
-//  */
-// github.addEventListener("click", () => {
-//   shell.openExternal("https://github.com/nastasagr/monitron");
-// });
-
-/**
- *   Updatable Callback
- */
 async function updateStats() {
   const cpuLoad = await si.currentLoad();
   const mem = await si.mem();
   const cpu = await si.cpu();
 
-  document.getElementById("cpu").innerText =
-    cpuLoad.currentLoad.toFixed(1) + "%";
+  const usedMemory = mem.total - mem.available;
+  const percent = ((usedMemory / mem.total) * 100).toFixed(1);
 
-  // document.getElementById("ram").innerText =
-  //   ((mem.used / mem.total) * 100).toFixed(1) + "%";
+  const systemData = {
+    cpu: {
+      load: Number(cpuLoad.currentLoad.toFixed(1)),
+      cores: cpu.cores,
+      manufacturer: cpu.manufacturer + " " + cpu.brand,
+    },
 
-  document.getElementById("model").innerText =
-    cpu.manufacturer + " " + cpu.brand;
+    memory: {
+      total: toGB(mem.total),
+      used: toGB(usedMemory),
+      free: toGB(mem.available),
+      usage: Number(percent),
+    },
+  };
 
-  document.getElementById("cores").innerText = cpu.cores;
-  document.getElementById("cache").innerText = cpu.governor
+  // cpu info elements
+  updateElement("cpu", "innerText", systemData.cpu.load + "%");
+  updateElement("cpuBar", "style.width", systemData.cpu.load + "%");
+  updateElement("model", "innerText", systemData.cpu.manufacturer);
+  updateElement("cores", "innerText", cpu.cores);
+
+  // memory info elements
+  updateElement("ramTotal", "innerText", systemData.memory.total + " GB");
+  updateElement("memUsed", "innerText", systemData.memory.usage + "%");
+  updateElement("ramBar", "style.width", systemData.memory.usage + "%");
+  updateElement("ramUsed", "innerText", systemData.memory.used + " GB");
+  updateElement("ramFree", "innerText", systemData.memory.free + " GB");
 }
 
 setInterval(updateStats, 1000);
